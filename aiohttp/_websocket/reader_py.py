fe775a2aff2eaa6d30cd893823a4881d57067812
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-08-24 19:10:33 +0800

    alvistack/3.14.3
        git clean -xdf
        git submodule sync --recursive
        git submodule update --recursive --init
        git submodule foreach --recursive git checkout -- .
        git submodule foreach --recursive git clean -xdf
        make generate-llhttp
        tar zcvf ../python-aiohttp_3.14.3.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-aiohttp.spec ../python-aiohttp_3.14.3-1.spec
        cp ../python*-aiohttp*3.14.3*.{gz,xz,spec,dsc} /osc/home\:alvistack/aio-libs-aiohttp-3.14.3/
        rm -rf ../python*-aiohttp*3.14.3*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- aiohttp/_websocket/reader_py.py
+++ aiohttp/_websocket/reader_py.py
@@ -19,7 +19,7 @@ from .models import (
     WSMsgType,
 )
 
-ALLOWED_CLOSE_CODES: Final[set[int]] = {int(i) for i in WSCloseCode}
+ALLOWED_CLOSE_CODES = {int(i) for i in WSCloseCode}
 
 # States for the reader, used to parse the WebSocket frame
 # integer values are used so they can be cythonized
@@ -328,7 +328,7 @@ class WebSocketReader:
         if self._tail:
             data, self._tail = self._tail + data, b""
 
-        start_pos: int = 0
+        start_pos = 0
         data_len = len(data)
         data_cstr = data
 
